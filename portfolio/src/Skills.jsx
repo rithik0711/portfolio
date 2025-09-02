375951d517a4da5022f1e5231c268ee42a3bc753
rithik0711 <rithikeswaran2005@gmail.com>
2025-09-02 21:15:16 +0530

    My portfolio

---
+++ portfolio/src/Skills.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import './Skills.css'
+
+const Skills = () => {
+  return (
+    <>
+    <div className='skill-main'>
+      <h2>Skills</h2>
+      <div className='skill-grid'>
+            <div className='skill-item'>
+              <img src="./src/assets/java.png" alt="Java" />
+              <span>Java</span>
+            </div>
+            <div className='skill-item'>
+              <img src="./src/assets/c.png" alt="C/C++" />
+              <span>C/C++</span>
+            </div>
+            <div className='skill-item'>
+              <img src="./src/assets/py.jpeg" alt="Python" />
+              <span>Python</span>
+            </div>
+            <div className='skill-item'>
+            <img src="./src/assets/js.png" alt="" />
+              <span>JavaScript</span>
+            </div>
+            <div className='skill-item'>
+              <img src="./src/assets/react.png" alt="React.js" />
+              <span>React.js</span>
+            </div>
+            <div className='skill-item'>
+              <img src="./src/assets/nextjs.jpeg" alt="" />
+              <span>Next.js</span>
+            </div>
+            <div className='skill-item'>
+              <img src="./src/assets/sql.jpg" alt="" />
+              <span>MySQL</span>
+            </div>
+            <div className='skill-item'>
+              <img src="./src/assets/mongodb.png" alt="" />
+              <span>MongoDB</span>
+            </div>
+          </div>
+    </div>
+    </>
+  )
+}
+
+export default Skills;
+
+
+<div className='skills'>
+        
+        
+        {/* Programming Languages Section */}
+        <div className='skill-category'>
+          {/* <h3>Programming Languages</h3> */}
+          
+        </div>
+        <div className='skill-category'>
+          {/* <h3>Frameworks & Libraries</h3> */}
+          <div className='skill-grid'>
+            
+          </div>
+        </div>
+        <div className='skill-category'>
+          {/* <h3>Databases</h3> */}
+          <div className='skill-grid'>
+            
+          </div>
+        </div>
+      </div>
